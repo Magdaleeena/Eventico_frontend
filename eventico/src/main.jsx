cba499f6395f5d01f881e741d5a809eada535cbf
Magdaleeena <magdalena.janeckova92@gmail.com>
2025-04-16 14:35:19 +0100

    updates

--- eventico/src/main.jsx
+++ eventico/src/main.jsx
@@ -1,13 +1,27 @@
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
 import ReactDOM from 'react-dom/client';
 import React from 'react';
+import App from './App';
 import './index.css';
+import { ClerkProvider } from '@clerk/clerk-react';
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ClerkProvider>
   </React.StrictMode>
 );
+
+
+
+
+
